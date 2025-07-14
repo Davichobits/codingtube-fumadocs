c878404c3cae6c53ed62f88fbd8d5f0bb5005a4c
Davichobits <davidruizdeveloper@gmail.com>
2025-07-14 05:53:58 -0500

    fix(tech-card): adjust layout and styling for improved responsiveness

--- components/tech-card.tsx
+++ components/tech-card.tsx
@@ -12,18 +12,20 @@ export const TechCard = ({
   return (
     <div
       data-card='true'
-      className='block rounded-xl border bg-fd-card p-4 text-fd-card-foreground transition-colors @max-lg:col-span-full max-w-[300px]'
+      className='rounded-xl border bg-fd-card p-4 text-fd-card-foreground transition-colors @max-lg:col-span-full max-w-[300px] max-h-[300px] flex flex-col justify-between'
     >
-      <div className='flex items-center gap-2'>
-        <div className='not-prose mb-2 rounded-lg bg-fd-muted p-1.5 text-fd-muted-foreground size-12 object-cover flex items-center justify-center [&>svg]:w-full [&>svg]:h-full'>
-          <Svg />
+      <div>
+        <div className='flex items-center gap-2'>
+          <div className='not-prose mb-2 rounded-lg bg-fd-muted p-1.5 text-fd-muted-foreground size-12 object-cover flex items-center justify-center [&>svg]:w-full [&>svg]:h-full'>
+            <Svg />
+          </div>
+          <h3 className='not-prose mb-1 text-sm font-medium'>{title}</h3>
         </div>
-        <h3 className='not-prose mb-1 text-sm font-medium'>{title}</h3>
+        <div className='text-sm text-fd-muted-foreground prose-no-margin empty:hidden mb-3'>
+          <p>{description}</p>
+        </div>
       </div>
 
-      <div className='text-sm text-fd-muted-foreground prose-no-margin empty:hidden mb-3'>
-        <p>{description}</p>
-      </div>
       <div className='flex justify-center gap-2'>
         {linkYoutube && (
           <Link
