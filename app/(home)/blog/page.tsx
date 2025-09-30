3bdf9f262d32a107b20062376bbad7cd18ede3d0
Davichobits <davidruizdeveloper@gmail.com>
2025-09-30 15:46:18 -0500

    Fix layout blogs

--- app/(home)/blog/page.tsx
+++ app/(home)/blog/page.tsx
@@ -15,13 +15,20 @@ export default function Home() {
       <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
         {posts.map((post, index) => (
             <article key={post.url} className='relative flex gap-5'>
-              <Image
-                className=''
-                src={`/assets/blog-images/blog${posts.length - index}.jpg`}
-                width={64 * 2}
-                height={96 * 2}
-                alt='code'
-              />
+              <Link
+                  href={post.url}
+                  className='block min-w-[128px] '
+                >
+                  <Image
+                    className='rounded-md'
+                    
+                    src={`/assets/blog-images/blog${posts.length - index}.jpg`}
+                    width={64 * 2}
+                    height={96 * 2}
+                    alt='code'
+                  />
+                </Link>
+              
 
               <div className='flex flex-col'>
                 <p className='text-4xl text-Hover font-extrabold'>{`0${
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Provider } from './provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { metadata } from './metada';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,6 +16,28 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="es" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-KH3JBESTXT"
+              strategy="afterInteractive"
+            />
+            <Script
+              id="gtag-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-KH3JBESTXT');
+                `,
+              }}
+            />
+            <Script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1562465173055929"
+              crossOrigin="anonymous"
+              strategy="afterInteractive"
+            />
         <Provider>{children}</Provider>
       </body>
     </html>
